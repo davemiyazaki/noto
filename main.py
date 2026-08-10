440637214606eaa60b68f6788afbd4c637a5862d
LinuxArch <denixden2818@gmail.com>
2026-08-10 19:32:56 -0400

    fix improper assignment of Dictionary

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from sudachipy import dictionary, tokenizer
 
-dict_obj = dictionary.Dictionary
+dict_obj = dictionary.Dictionary()
 
-tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos")
+tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos"})
+
+raw_text ="私はせったちでごめん、ただ話したいなの"
+listOfSomeSorts = tokenizer_obj.tokenize(raw_text)
+
+for m in listOfSomeSorts :
+    print(m)
