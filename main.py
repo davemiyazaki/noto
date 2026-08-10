2463b91d80529a26f14789b97d15c6ca761202f7
LinuxArch <denixden2818@gmail.com>
2026-08-10 18:50:01 -0400

    Set up tokenizer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from sudachi import dictionary, tokenizer
+from sudachipy import dictionary, tokenizer
 
 dict_obj = dictionary.Dictionary
+
+tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos")
