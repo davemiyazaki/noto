6d55a2f5284550c7dcdd275a61adef0d3a32678b
LinuxArch <denixden2818@gmail.com>
2026-08-10 19:34:36 -0400

    fix sentence grammar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ dict_obj = dictionary.Dictionary()
 
 tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos"})
 
-raw_text ="私はせったちでごめん、ただ話したいなの"
+raw_text ="私はせっかちでごめん、ただ話したいなの"
 listOfSomeSorts = tokenizer_obj.tokenize(raw_text)
 
 for m in listOfSomeSorts :
