1722c4cf35ecafd1809a180f03cd7ededd856e19
LinuxArch <denixden2818@gmail.com>
2026-08-12 12:30:22 -0400

    Test refact

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos"})
 raw_text ="私はせっかちでごめん、ただ話したいなの"
 listOfSomeSorts = tokenizer_obj.tokenize(raw_text)
 
+listOfWords = {}
+
 for m in listOfSomeSorts :
-    print(f"Instance in the sentence {m.surface()}")
-    print(f"Dictionary form: {m.dictionary_form()}")
-    print(f"Part of Speech: {m.part_of_speech()[0]}")
+
+    wordInstance =[m.surface(), m.dictionary_form(), m.part_of_speech()]
+    print(wordInstance)
