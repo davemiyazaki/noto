6ded37e29057dd1f9a3f968cf337875f20fb77cf
LinuxArch <denixden2818@gmail.com>
2026-08-13 18:33:42 -0400

    Comment out sudachiPy code to start fastAPI

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from sudachipy import dictionary
+#from sudachipy import dictionary
+#dict_obj = dictionary.Dictionary()
+#tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos"})
+#raw_text ="私はせっかちでごめん、ただ話したいなの"
+#listOfSomeSorts = tokenizer_obj.tokenize(raw_text)
+#listOfWords = {}
+#for m in listOfSomeSorts :
 
-dict_obj = dictionary.Dictionary()
-
-tokenizer_obj = dict_obj.create(fields={"surface","dictionary_form","pos"})
-
-raw_text ="私はせっかちでごめん、ただ話したいなの"
-listOfSomeSorts = tokenizer_obj.tokenize(raw_text)
-
-listOfWords = {}
-
-for m in listOfSomeSorts :
-
-    wordInstance =[m.surface(), m.dictionary_form(), m.part_of_speech()]
-    print(wordInstance)
+#    wordInstance =[m.surface(), m.dictionary_form(), m.part_of_speech()]
+#    print(wordInstance)
