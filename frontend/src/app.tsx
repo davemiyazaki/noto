957e35591bb53711cd605d735e6bd2bb8bd42a10
LinuxArch <denixden2818@gmail.com>
2026-08-13 20:04:57 -0400

    add button, add method to send, add interface for response

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,10 +2,28 @@
 //import "./app.css";
 import { createSignal } from "solid-js";
 
+interface ItemResponse {
+  message: string;
+  data: {
+    var: string;
+  }
+}
+
+
 export default function App() {
 
   const [rawText, setRawText] = createSignal<string>("")
+  async function sendText(): Promise<void> {
+    try {
+      const res = await fetch("http:localhost:8000/echo/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ var: rawText() }),
+      });
+    }
 
+    c
+  }
   return (
     <div class="main">
       <div class="text">Hello World</div>
@@ -13,7 +31,8 @@ export default function App() {
         type="text"
         onInput={(e: InputEvent & { target: HTMLInputElement }) => setRawText(e.target.value)}
         placeholder="Raw Text"
-        />
+      />
+      <button onClick={sendText()}>Send</button>
     </div>
   );
 }
